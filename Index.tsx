ad52751ab9ebca5b3972c8c67ed1110ee0cbb5b9
ENCIholding <hello@estatenest.capital>
2025-11-08 00:02:40 -0700

    Create Index.tsx

---
+++ Index.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import ContentSection from "@/components/ContentSection";
+import AboutUs from "@/components/AboutUs";
+import ProjectManagement from "@/components/ProjectManagement";
+import BuilderCredentials from "@/components/BuilderCredentials";
+import AppointmentSection from "@/components/AppointmentSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <HeroSection />
+      <ContentSection />
+      <AboutUs />
+      <ProjectManagement />
+      <BuilderCredentials />
+      <AppointmentSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
